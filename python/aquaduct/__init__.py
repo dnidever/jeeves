3b0d7a26ac03c85d44553637a788f4a8824e94f9
David Nidever <dnidever@montana.edu>
2024-04-11 23:35:48 -0400

    adding initial python files

---
+++ python/aquaduct/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ["utils","jeeves"]
+__version__ = "1.0.0"
---
+++ python/aquaduct/jeeves.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+
+
+# Ideas
+# -configuration file that specifies the paths and filenames
+#   allow the use of code but with special symbols denoting it
+#   allow for using a tag that specifies it's a function (like in sdss_access)
+# -how about reading/writing?  just to/from FITS?  return hdu?
+# -should we save meta-data to a database and allow for it to be searched?
+#   I think the LSST butler does this
+
+# figure out how I would use jeeves for my various pipelines
+#  delve, nsc, apogee, etc.
+
+# Ideas from PHOTRED/MAPS, SMASH, DELVE, APOGEE/SDSS, NSC, LSST, JWST
